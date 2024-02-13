d9dbb4fa2cebda80a2e626df018de3b4a6d7f1cd
Amakiri David <120092804+Amakiri201@users.noreply.github.com>
2024-02-13 03:58:21 +0100

    Add files via upload
    update for responsiveness

--- js/script.js
+++ js/script.js
@@ -17,26 +17,29 @@ document.addEventListener("DOMContentLoaded", function () {
     did_open: false,
   };
 
-  const message = document.querySelector("#message");
   const heart = document.querySelector("#solid-heart");
   const heart_container = document.querySelector(".heart-container");
+
   const msg_container = document.querySelector("#message-container");
 
   const md = window.markdownit({ html: true });
 
   loadMessage(md);
 
+  // Function to toggle the message container
   heart.addEventListener("click", function () {
-    if (tempdata.did_open) {
-      return;
+    if (!tempdata.did_open) {
+      // Open the message container
+      msg_container.style.display = "block";
+      heart_container.style.marginTop = "-1.5%";
+
+      const auto_css_height = msg_container.scrollHeight + 0; // Calculate the height of the content
+      msg_container.style.maxHeight = auto_css_height + "px";
+      tempdata.did_open = true;
+    } else {
+      // Close the message container
+      msg_container.style.maxHeight = "0"; // Reset max-height to close the message container
+      tempdata.did_open = false;
     }
-    tempdata.did_open = true;
-
-    msg_container.style.display = "block";
-    heart_container.style.marginTop = "-1.5%";
-
-    const auto_css_height = message.scrollHeight + 20; // 20 extra padding
-    // msg_container.scrollHeight + 20; // 20 extra padding
-    msg_container.style.height = auto_css_height + "px";
   });
 });
